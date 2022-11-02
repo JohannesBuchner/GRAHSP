30a5a409270b4864aee500262adc9284a93d20cc
Johannes Buchner <johannes.buchner.acad@gmx.com>
2022-11-02 17:36:51 +0100

    Bump version: 0.8.0 → 0.8.1

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ entry_points = {
 
 setup(
     name="grahsp",
-    version="0.8.0",
+    version="0.8.1",
     packages=find_packages(exclude=["database_builder"]),
 
     install_requires=['numpy', 'scipy', 'sqlalchemy', 'matplotlib',
