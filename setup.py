f2e96b164979ae1e268c1dfa4b1f356e47959df2
Johannes Buchner <johannes.buchner.acad@gmx.com>
2024-08-27 14:02:09 +0200

    Bump version: 1.2.5 → 1.3.0

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ entry_points = {
 
 setup(
     name="grahsp",
-    version="1.2.5",
+    version="1.3.0",
     packages=find_packages(exclude=["database_builder"]),
 
     install_requires=['numpy', 'scipy', 'sqlalchemy', 'matplotlib',
